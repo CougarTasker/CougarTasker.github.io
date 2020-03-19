df566e797da3b3a03dacf5b09fe5d8decebdd8f3
cougartasker <cougartasker@outlook.com>
2020-03-19 19:42:09 +0000

    now you can add comments

--- ventaway/comments.js
+++ ventaway/comments.js
@@ -1,29 +1,16 @@
-var comments = {
-	text:"",
-	id:-1,
-	sub:[{
-	id:0,
-	text:"this is a comment",
-	sub:[{id:1,text:"this is a sub comment",sub:[]},{id:2,text:"this is another sub comment",sub:[{id:3,text:"this is a sub comment",sub:[]},{id:4,text:"this is another sub comment",sub:[]}]}]
-}]
-};
-
-var small = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Odio euismod lacinia at quis risus sed vulputate. Vitae et leo duis ut diam quam nulla. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Ante metus dictum at tempor commodo ullamcorper a. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Facilisis leo vel fringilla est ullamcorper eget nulla facilisi. A scelerisque purus semper eget duis. Sed turpis tincidunt id aliquet risus. Mi quis hendrerit dolor magna. Volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris. Ornare quam viverra orci sagittis eu volutpat odio facilisis mauris. Quis ipsum suspendisse ultrices gravida dictum fusce ut."
-var med = small+"<br /><br />" + "Tellus cras adipiscing enim eu. Purus semper eget duis at tellus at urna condimentum. Varius vel pharetra vel turpis nunc. Nibh sed pulvinar proin gravida hendrerit. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit. Nisi porta lorem mollis aliquam. Dis parturient montes nascetur ridiculus mus mauris vitae. Sed arcu non odio euismod lacinia at quis risus sed. Facilisis gravida neque convallis a cras semper auctor neque. Pellentesque diam volutpat commodo sed egestas egestas fringilla. Risus in hendrerit gravida rutrum quisque non tellus orci. Tortor vitae purus faucibus ornare. Luctus accumsan tortor posuere ac ut consequat. Massa ultricies mi quis hendrerit dolor magna eget. Ut etiam sit amet nisl. Fermentum et sollicitudin ac orci phasellus egestas tellus. Eu sem integer vitae justo.";
-var long = med +"<br /><br />" + "Platea dictumst quisque sagittis purus sit amet. Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Id venenatis a condimentum vitae. Dictum at tempor commodo ullamcorper a lacus. Quis auctor elit sed vulputate mi sit amet mauris. Quis vel eros donec ac odio tempor orci dapibus. Consequat id porta nibh venenatis cras. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Feugiat sed lectus vestibulum mattis ullamcorper velit sed. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Sapien nec sagittis aliquam malesuada. Porttitor massa id neque aliquam vestibulum morbi."
-var comments2 = {id:10,text:"",sub:[{id:11,text:"diffrent comments",sub:[]},{id:12,text:small,sub:[{id:13,text:"that was a long one",sub:[]}]},{id:14,text:"like this one",sub:[]}]}
-vents = [{text:small,image:"img_chania.jpg",alt:"Flowers in Chania",comments:comments},{text:med,image:"",comments:comments2},{text:long,image:"img_chania.jpg",alt:"Flowers in Chania",comments:comments}];
+var vents = [{id:"0",text:"one vent",image:"img_chania.jpg",alt:"some corona",comments:{id:"0_0",text:"",sub:[]}}]
 var input = new usrInput();
+var ventWrappers = []
 $(document).ready(function(){
 	for(vent in vents){
 		var v = new ventWrapper(vents[vent]);
 		$(".container").append(v.generate());
-		v.addHandelers();
+			v.addHandelers();
+		ventWrappers.push(v);
 	}
 	autosize($('textarea'));
 	input.addHandelers();
 });
-
 function usrInput(){
 	this.addHandelers = function(){
 		$(".userinput #submitForm").click(this.sendHandler);
@@ -31,38 +18,46 @@ function usrInput(){
 		$(".userinput #responce").change(this.modeChange);
 	};
 	this.modeChange = function(event){
-		var mode = $(event.target)
+		var mode = $(event.target);
 		var txt = mode.siblings("textarea");
-		console.log("hello");
 		txt.val("");
+		txt.focus();
 		if(mode.val() < 0){
 			txt.attr("placeholder","Send your vent");
 		}else{
 			txt.attr("placeholder","Comment");
 		}
+
 	}
 	this.sendHandler = function(){
-		var url = "/";
-	    $.ajax({
-	           type: "POST",
-	           url: url,
-	           data: $(".userinput>form").serialize(),
-	           success: function(data)
-	           {
-	               alert(data); 
-	           }
-	         });
+		var message = $(".userinput textarea").val().trim();
+		if(message != ""){
+			var url = ".";
+		    $.ajax({
+		           type: "POST",
+		           url: url,
+		           data: $(".userinput>form").serialize(),
+		           success: function(data)
+		           {
+		               alert(data); 
+		           }
+		         });
+		    var res = $(".userinput #responce").val()
+		    if(res > 0){
+			    new commentWrapper({text:"",id:"",sub:[]}).addComment($(".userinput #responce").val(),message);
+		 	   $(".userinput textarea").val("");
+		 	   $(".userinput #responce").val("-1").change();
+		    }else{
+		    	//new ventWrapper({text:"",id:"",image:"",alt:"",comments:[]}).addVent(message)
+		    }
 
+		}
     	return false; // avoid to execute the actual submit of the form.
 	}
 	this.attachHandler = function(){
 		$("#img").click();
 	}
 }
-
-
-
-
 function commentWrapper(comments){
 	this.comments = comments
 	this.expandClick = function(event){
@@ -70,18 +65,25 @@ function commentWrapper(comments){
 		t.hide();
 		t.parents(".comment").removeClass("hidden");
 	};
+	this.addComment = function(parent,message){
+		$("#COM"+parent).append(this.generate({text:message,id:parent+"_"+(this.max(parent)),sub:[]}));
+		this.addHandelers();
+	}
+	this.max = function(id){
+		return $("#COM"+id).children(".comment").length;
+	}
 	this.controlClick = function(event){
 	var elem =  $(event.target);
 	switch(elem.text()){
 		case "mode_comment":
-			$(".userinput#responce").val(elem.closest(".comment").attr('id').substring(3)).change();
+			$(".userinput #responce").val(elem.closest(".comment").attr('id').substring(3)).change();
 		break;
 		case "arrow_downward":
-			console.log("comm");
-			elem.toggleClass("forange");
+			
+			elem.parent().toggleClass("forange");
 		break;
 		case "arrow_upward":
-			elem.toggleClass("forange");
+			elem.parent().toggleClass("forange");
 		break;
 		}
 	} 	
@@ -128,6 +130,10 @@ function commentWrapper(comments){
 function ventWrapper(vent){
 	this.vent = vent;
 	this.comments = new commentWrapper(vent.comments);
+	this.addVent = function(parent,message){
+		$("#COM"+parent).append(this.generate({text:message,id:parent+"_"+(this.max(parent)),sub:[]}));
+		this.addHandelers();
+	}
 	this.generate = function(){
 		var message = `<div class="post bgreyl round">
 							<div class="message">`;
