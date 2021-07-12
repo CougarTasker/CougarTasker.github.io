91e49399ddefc4406b052a03baff67b37cc1a3a1
Cougar Tasker <58560033+CougarTasker@users.noreply.github.com>
2021-07-12 22:33:42 +0100

    added placeholder snake parts.

--- projects/autosnake/snake.js
+++ projects/autosnake/snake.js
@@ -14,7 +14,7 @@ const colors = {
 
 const gameDimentions = { x: 10, y: 10 };
 
-drawInstance = ({ }) => {
+drawInstance = ({ snake, progress }) => {
   offset = {
     x: Math.floor(canvas.width % gameDimentions.x / 2),
     y: Math.floor(canvas.width % gameDimentions.x / 2),
@@ -36,25 +36,42 @@ drawInstance = ({ }) => {
       ctx.fillRect(cellSize.x * x + offset.x, cellSize.y * y + offset.y, cellSize.x, cellSize.y);
     }
   }
+  const snakeTailSize = 0.4;
+  const snakeHeadSize = 0.9;
+  const snakeStepSize = (snakeHeadSize - snakeTailSize) / snake.length;
+  const snake 
+
+  let size = snakeTailSize;
+
+  snake.forEach(part => {
+    ctx.fillStyle = colors.green;
+    ctx.beginPath();
+    ctx.arc(
+      cellSize.x * part.x + offset.x + cellSize.x / 2,
+      cellSize.y * part.y + offset.y + cellSize.y / 2,
+      cellSize.x * 0.5 * size, 0, 2 * Math.PI);
+    ctx.fill();
+    size += snakeStepSize;
+  });
 }
 
 
-const setCanvasSize = ()=>{
+const setCanvasSize = () => {
   let size = Math.floor(canvasContainer.clientWidth);
   const maxSize = Math.floor((window.innerHeight - 70) * 0.9);
-  if(size> maxSize){
+  if (size > maxSize) {
     size = maxSize;
   }
   canvas.width = size;
   canvas.height = size;
 }
 
-window.addEventListener("resize",setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 setCanvasSize();
 
 const renderLoop = () => {
 
-  drawInstance({});
+  drawInstance({ snake: [{ x: 3, y: 1 }, { x: 4, y: 1 }, { x: 5, y: 1 }] });
   window.requestAnimationFrame(renderLoop);
 }
 renderLoop();
