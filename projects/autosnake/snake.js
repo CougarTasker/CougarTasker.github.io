f1534752bcd3584357d3263109668e9fe255c88b
Cougar Tasker <58560033+CougarTasker@users.noreply.github.com>
2021-07-13 15:20:47 +0100

    clipping the missing part of the tail.

--- projects/autosnake/snake.js
+++ projects/autosnake/snake.js
@@ -62,7 +62,25 @@ computeDirections = (snake, nextHead) => {
   }
   return out;
 }
-drawRightCorner = (cell, startSize, endSize) => {
+//http://www.independent-software.com/determining-coordinates-on-a-html-canvas-bezier-curve.html
+
+function getBezierXY(t, sx, sy, cp1x, cp1y, cp2x, cp2y, ex, ey) {
+  return {
+    x: Math.pow(1 - t, 3) * sx + 3 * t * Math.pow(1 - t, 2) * cp1x
+      + 3 * t * t * (1 - t) * cp2x + t * t * t * ex,
+    y: Math.pow(1 - t, 3) * sy + 3 * t * Math.pow(1 - t, 2) * cp1y
+      + 3 * t * t * (1 - t) * cp2y + t * t * t * ey
+  };
+}
+
+drawCorner = (cell, progress, snakeLenght) => {
+  //if this curve includes the tail 
+
+
+
+
+  startSize = lerp(snakeTailSize, snakeHeadSize, (cell.order - 0.5 - progress) / snakeLenght),
+    endSize = lerp(snakeTailSize, snakeHeadSize, (cell.order + 0.5 - progress) / snakeLenght)
   const innerStrength = 0.5;
   const outerStrength = 0.5;
   const center = {
@@ -77,6 +95,7 @@ drawRightCorner = (cell, startSize, endSize) => {
     x: center.x + cell.dir.x / 2,
     y: center.y + cell.dir.y / 2
   }
+
 
   const centerT = transform(center);
   const startPosT = transform(startPos);
@@ -102,12 +121,7 @@ drawRightCorner = (cell, startSize, endSize) => {
     y: startPos.y + cell.dir.y * startSize / 2 + cell.preDir.y * (0.5 - endSize / 2) * innerStrength
   })
 
-  ctx.beginPath();
-  ctx.moveTo(endPosR.x, endPosR.y);
-  ctx.bezierCurveTo(
-    endPosRGuide.x, endPosRGuide.y,
-    startPosRGuide.x, startPosRGuide.y,
-    startPosR.x, startPosR.y);
+  
 
   //create the outer side
   //start posL to end posL
@@ -131,18 +145,61 @@ drawRightCorner = (cell, startSize, endSize) => {
   })
 
 
+
+  ctx.save();
+  if (cell.order == 1 && progress >= 0.5) {
+    ctx.beginPath();
+    const cutofR = getBezierXY(
+      progress - 0.5,
+      startPosR.x, startPosR.y,
+      startPosRGuide.x, startPosRGuide.y,
+      endPosRGuide.x, endPosRGuide.y,
+      endPosR.x, endPosR.y)
+    const cutofL = getBezierXY(
+      progress - 0.5,
+      startPosL.x, startPosL.y,
+      startPosLGuide.x, startPosLGuide.y,
+      endPosLGuide.x, endPosLGuide.y,
+      endPosL.x, endPosL.y)
+    const endPosCellL = transform({
+      x: endPos.x + cell.preDir.x / 2,
+      y: endPos.y + cell.preDir.y / 2
+    });
+    const startPosCellR = transform({
+      x: startPos.x + cell.dir.x / 2,
+      y: startPos.y + cell.dir.y / 2
+    });
+    const corner = transform({
+      x: center.x - cell.dir.x / 2 + cell.preDir.x / 2,
+      y: startPos.y - cell.dir.y / 2 + cell.preDir.y / 2
+    });
+
+    ctx.moveTo(cutofR.x, cutofR.y);
+    ctx.lineTo(cutofL.x, cutofL.y);
+    ctx.lineTo(corner.x, corner.y);
+    ctx.lineTo(endPosCellL.x, endPosCellL.y);
+    ctx.lineTo(startPosCellR.x, startPosCellR.y);
+    ctx.clip();
+  }
+  ctx.beginPath();
+  ctx.moveTo(endPosR.x, endPosR.y);
+  ctx.bezierCurveTo(
+    endPosRGuide.x, endPosRGuide.y,
+    startPosRGuide.x, startPosRGuide.y,
+    startPosR.x, startPosR.y);
   ctx.lineTo(startPosL.x, startPosL.y);
   ctx.bezierCurveTo(
     startPosLGuide.x, startPosLGuide.y,
     endPosLGuide.x, endPosLGuide.y,
     endPosL.x, endPosL.y);
   ctx.fill();
-
+  ctx.restore();
 }
 
-drawCorner = (cell, startSize, endSize) => {
-  drawRightCorner(cell, startSize, endSize);
-}
+
+
+
+
 // i am going to create a grid cordinates to canvas coridnates function 
 const transform = ({ x: xin, y: yin, width: widthin, height: heightin }) => {
   offset = {
@@ -160,6 +217,9 @@ const transform = ({ x: xin, y: yin, width: widthin, height: heightin }) => {
     height: cellSize.y * (heightin ?? 0)
   }
 }
+const snakeTailSize = 0.4;
+const snakeHeadSize = 0.8;
+
 drawInstance = ({ snake }, { snake: nextSnake }, progress) => {
 
   //draw grid
@@ -177,8 +237,7 @@ drawInstance = ({ snake }, { snake: nextSnake }, progress) => {
       ctx.fillRect(box.x, box.y, box.width, box.height);
     }
   }
-  const snakeTailSize = 0.4;
-  const snakeHeadSize = 0.8;
+
   const snakeStepSize = (snakeHeadSize - snakeTailSize) / (snake.length - 1);
 
   let size = snakeTailSize;
@@ -212,14 +271,26 @@ drawInstance = ({ snake }, { snake: nextSnake }, progress) => {
     size += snakeStepSize;
   });
 
-
-  corners.forEach(corner=>{
-    drawCorner(corner,
-      lerp(snakeTailSize, snakeHeadSize, (corner.order - 0.5 - progress) / snake.length),
-      lerp(snakeTailSize, snakeHeadSize, (corner.order + 0.5 - progress) / snake.length)
-    );
+  ctx.fillStyle = colors.green;
+  //tail
+  ctx.beginPath();
+  const box = transform({
+    x: (tail.x + tail.dir.x * progress) + 0.5,
+    y: (tail.y + tail.dir.y * progress) + 0.5,
+    width: 0.5 * snakeTailSize,
   })
-  
+  ctx.arc(
+    box.x,
+    box.y,
+    box.width,
+    tail.dir.angle + Math.PI / 2,
+    tail.dir.angle - Math.PI / 2);
+  ctx.fill();
+
+  corners.forEach(corner => {
+    drawCorner(corner, progress, snake.length);
+  })
+
 
 }
 
