de4a7e36f86b691c294f4df7a5b9fbc1c0908619
Cougar Tasker <58560033+CougarTasker@users.noreply.github.com>
2021-07-13 10:00:07 +0100

    added a moving head and tail

--- projects/autosnake/snake.js
+++ projects/autosnake/snake.js
@@ -13,8 +13,11 @@ const colors = {
 };
 
 const gameDimentions = { x: 10, y: 10 };
+lerp = (start, end, factor) => {
+  return start + (end - start) * factor;
+}
 
-drawInstance = ({ snake, progress }) => {
+drawInstance = ({ snake }, { snake: nextSnake }, progress) => {
   offset = {
     x: Math.floor(canvas.width % gameDimentions.x / 2),
     y: Math.floor(canvas.width % gameDimentions.x / 2),
@@ -37,9 +40,8 @@ drawInstance = ({ snake, progress }) => {
     }
   }
   const snakeTailSize = 0.4;
-  const snakeHeadSize = 0.9;
-  const snakeStepSize = (snakeHeadSize - snakeTailSize) / snake.length;
-  const snake 
+  const snakeHeadSize = 0.8;
+  const snakeStepSize = (snakeHeadSize - snakeTailSize) / (snake.length - 1);
 
   let size = snakeTailSize;
 
@@ -53,6 +55,39 @@ drawInstance = ({ snake, progress }) => {
     ctx.fill();
     size += snakeStepSize;
   });
+
+  //add moving head and tail
+  const tail = snake[0];
+  const head = snake[snake.length - 1];
+  const nextTail = nextSnake[0];
+  const nextHead = nextSnake[nextSnake.length - 1];
+
+  //head 
+  midHead = {
+    x: lerp(head.x, nextHead.x, progress),
+    y: lerp(head.y, nextHead.y, progress)
+  }
+  ctx.fillStyle = colors.red;
+  ctx.beginPath();
+  ctx.arc(
+    cellSize.x * midHead.x + offset.x + cellSize.x / 2,
+    cellSize.y * midHead.y + offset.y + cellSize.y / 2,
+    cellSize.x * 0.5 * snakeHeadSize, 0, 2 * Math.PI);
+  ctx.fill();
+
+
+  //tail 
+  midTail = {
+    x: lerp(tail.x, nextTail.x, progress),
+    y: lerp(tail.y, nextTail.y, progress)
+  }
+  ctx.fillStyle = colors.red;
+  ctx.beginPath();
+  ctx.arc(
+    cellSize.x * midTail.x + offset.x + cellSize.x / 2,
+    cellSize.y * midTail.y + offset.y + cellSize.y / 2,
+    cellSize.x * 0.5 * snakeTailSize, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
 
@@ -69,9 +104,15 @@ const setCanvasSize = () => {
 window.addEventListener("resize", setCanvasSize);
 setCanvasSize();
 
-const renderLoop = () => {
+const fakeCurrentInstance = { snake: [{ x: 3, y: 1 }, { x: 4, y: 1 }, { x: 5, y: 1 }] }
+const fakeNextInstance = { snake: [{ x: 4, y: 1 }, { x: 5, y: 1 }, { x: 6, y: 1 },] }
 
-  drawInstance({ snake: [{ x: 3, y: 1 }, { x: 4, y: 1 }, { x: 5, y: 1 }] });
+let start = Date.now();
+const renderLoop = () => {
+  const progressDuration = 1000;
+  const progress = ((Date.now() - start) % progressDuration) / progressDuration;
+  drawInstance(fakeCurrentInstance, fakeNextInstance, progress);
   window.requestAnimationFrame(renderLoop);
+
 }
 renderLoop();
